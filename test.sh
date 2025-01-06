3c7d2d00049fd99d810dacde413c122029776f79
Greg Brandt <brandt.greg@gmail.com>
2025-01-05 20:21:59 -0800

    WIP

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# TODO(greg_brandt) Use nosetests or something
-
-./bin/jdoc org.apache
-./bin/jdoc --group org.apache.httpcomponents --artifact httpclient --version 4.5.3
-./bin/jdoc 'com.(airbnb|google)'
-./bin/jdoc -c HttpClient
-./bin/jdoc org.apache.http.client.HttpClient
-./bin/jdoc org.apache.http.client.HttpClient --show_source
